e4ddaa5689b6514e4f6617095564bb975b7508c2
Ayushcode10 <ayushh.code@gmail.com>
2025-05-22 20:59:06 +0530

    Remove .env and add to .gitignore

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "7c8fa3b5f926638a423cead0c5b23489";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
